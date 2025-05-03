5cd7b8e67fa4a1a484d1fc7a5dd07de0f21b3f71
Roman Malashenkov <malashenkov.roma@gmail.com>
2025-05-03 16:42:14 +0300

    ..

--- internal/adapters/api/methods.go
+++ internal/adapters/api/methods.go
@@ -3,7 +3,7 @@ package api
 import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
 func (a *Api) SendMessage(msg string) error {
-	msgConfig := tgbotapi.NewMessage(a.cfg.ChatID, msg)
+	msgConfig := tgbotapi.NewMessage(1, msg) //
 
 	_, err := a.bot.Send(msgConfig)
 
